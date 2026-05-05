b59cee1b4a6ac3236a8bec1da026416ae8e93c8a
iqraaslam123 <iqraaslam1966@gmail.com>
2026-05-05 22:14:17 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
+export default function App() {
+ return (
+ <BrowserRouter>
+ <AuthProvider>
+ <Routes>
+ <Route path='/' element={<Login />} />
+ <Route path='/login' element={<Login />} />
+ <Route path='/register' element={<Register />} />
+ <Route path='/dashboard' element={
+ <PrivateRoute><Dashboard /></PrivateRoute>
+ } />
+ </Routes>
+ </AuthProvider>
+ </BrowserRouter>
+ );
+}
