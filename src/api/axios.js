5fc15c4861de9046e4c2b3f9da3900b4de03cc29
iqraaslam123 <iqraaslam1966@gmail.com>
2026-05-05 22:50:53 +0500

    running frontend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,15 @@
+// import axios from 'axios';
+
+// const api = axios.create({
+//   baseURL: 'https://auth-app-backend-inky.vercel.app/api', // apna backend port yahan likhو
+// });
+
+// export default api;
+
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // apna backend port yahan likhو
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 });
 
 export default api;
