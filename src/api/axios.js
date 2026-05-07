a93f09a847b915fa4a94bd64e8047527585f396e
iqraaslam123 <iqraaslam1966@gmail.com>
2026-05-07 21:01:18 +0500

    fix api url

--- src/api/axios.js
+++ src/api/axios.js
@@ -6,10 +6,19 @@
 
 // export default api;
 
+// import axios from 'axios';
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL
+// });
+
+// export default api;
+
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
 
-export default api;
+export default API;
